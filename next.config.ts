832e0c4af9dac981d3524f1c5d450ea3646efc40
Shreyas Mishra <shreyasmishra03@gmail.com>
2025-02-01 00:35:02 +0530

    init

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+    /* config options here */
+    output: 'standalone',
+    distDir: "build",
 };
 
 export default nextConfig;
